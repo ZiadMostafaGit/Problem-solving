1bd6ea061c9a0ce175536ad9160fde093dfbac98
ZiadMostafagit <mygithub2003@gmail.com>
2024-03-29 06:55:51 -0700

    update

---
+++ LeetCode/Reverse Integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        string=str(x)
+        j=1
+
+        if x<0:
+          
+         string=string[1:]
+         j=-1
+
+        newstr=""
+        for i in range(len(string)-1,-1,-1):
+         newstr+=string[i]
+         
+        if int(newstr)>(2**31)-1:
+           return 0
+        return int(newstr)*j  
+
